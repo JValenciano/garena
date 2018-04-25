1d18f22706a1bf3a21a6cfebac926655f67ee935
unknown <jhavlncn@gmail.com>
2018-04-25 16:45:50 +0800

    GPL Copy

--- public_html/lol-home/js/main.js
+++ public_html/lol-home/js/main.js
@@ -1,3 +1,10 @@
+import $ from "jquery";
+import jQueryBridget from "jquery-bridget";
+import Flickity from "flickity";
+// make Flickity a jQuery plugin
+jQueryBridget( 'flickity', Flickity, $ );
+// now you can use $().flickity()
+
 $(document).ready(function(){
   $('#wishlist a').click(function(e){
     e.preventDefault();
